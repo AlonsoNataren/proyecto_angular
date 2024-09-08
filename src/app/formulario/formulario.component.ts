21868b385ec14fffd11c9822544534066a8de368
Alonso Nataren <luisalonso14@hotmail.com>
2024-09-08 14:58:32 -0600

    formulario manda a consola

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  mostrar_en_consola(name:string){
+    console.log(name);
+  }
 
 }
